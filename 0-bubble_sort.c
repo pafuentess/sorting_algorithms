494f0afa1658f492f5312403c1c6d8a0f597810f
pafuentess <pafuentess@unal.edu.co>
2020-01-30 16:43:48 -0500

    first commit

---
+++ 0-bubble_sort.c
@@ -0,0 +1,28 @@
+#include "sort.h"
+#include <stdio.h>
+
+void bubble_sort(int *array, size_t size)
+{
+	unsigned int i, count = 0;
+	int tmp = 0;
+	size_t flag = 0;
+
+	
+	count = size;
+	while (count > 0)
+	{
+		flag = 0;
+		for (i = 0 ; i < count - 1 ; i++)
+		{
+			if (array[i] > array[i + 1])
+			{
+				tmp = array[i + 1];
+				array[i + 1] = array[i];
+				array[i] = tmp;
+				print_array(array, size);
+				flag = i + 1;
+			}
+		}
+		count = flag;
+	}
+}
---
+++ sort.h
@@ -0,0 +1,11 @@
+#ifndef FUNCTION_POINTERS_H
+#define FUNCTION_POINTERS_H
+#include <stdlib.h>
+#include <stdio.h>
+
+
+
+void print_array(const int *array, size_t size);
+void bubble_sort(int *array, size_t size);
+
+#endif
