c118f9528c930a57ebe1da12353cd003da7d9abc
pafuentess <pafuentess@unal.edu.co>
2020-01-31 16:40:56 -0500

    third commit

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -4,35 +4,51 @@
 
 void insertion_sort_list(listint_t **list)
 {
-	listint_t *tmp1, *tmp2;
+	listint_t *tmp1, *tmp2, *reverse, *cpy;
 
 	tmp1 = *list;
 	tmp2 = tmp1->next;
 
 	while (tmp2->next)
 	{
+		printf("tmp1 = %d, tmp2 = %d\n", tmp1->n, tmp2->n);
 		if (tmp1->n < tmp2->n)
-		{
-			while(tmp1->prev != NULL)
-			{
-				if (tmp1->n > tmp2->n)
-				{
-					tmp1 = tmp1->prev;
-				}
-				else
-				{
-					tmp2->next = tmp1;
-					tmp2->prev = tmp1->prev;
-					print_list(*list);
-				}
-				
-			}
-		}
+                        printf("true\n");
 		else
 		{
-			tmp1 = tmp1->next;
-			tmp2 = tmp2->next;
+			reverse = tmp1->prev;
+			printf("reverse %d\n", reverse->n);
+			printf("false\n");
+			while (reverse->n < tmp2->n && reverse->prev != NULL)
+			{
+				printf("entro al while\n");
+				if (reverse->prev != NULL)
+					reverse = reverse->prev;
+				printf("reverse %d\n", reverse->n);
+			}
+			cpy = tmp2;
+			tmp2->next = tmp1;
+			tmp2->prev = reverse;
+			reverse->next = tmp2;
+			tmp1->prev = tmp2;
+			tmp1->next = cpy->next;
+			cpy->next = tmp1;
+			print_list(*list);
 		}
-		
+		tmp1 = tmp1->next;
+		tmp2 = tmp2->next;
 	}
 }
+
+/*
+if (tmp2->n < reverse->n && tmp2->n > reverse->prev->n)
+                                        printf("%d entre estos dos cabe el nodo\n", i);
+                                else
+                                {
+                                        printf("tengo que mover reverse\n");
+                                        if (reverse->prev != NULL)
+                                                reverse = reverse->prev;
+                                        printf("valor reverse %d\n", reverse->n);
+                                        break;
+                                }
+*/
