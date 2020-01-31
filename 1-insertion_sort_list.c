c44644978c7af790d8fe9b67e1b55233beced3e3
andreammgcol <amendez72@misena.edu.co>
2020-01-31 14:59:35 -0500

    sort

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,31 +1,38 @@
 #include "sort.h"
-
+#include <stdio.h>
 
 
 void insertion_sort_list(listint_t **list)
 {
 	listint_t *tmp1, *tmp2;
-	int number;
 
 	tmp1 = *list;
 	tmp2 = tmp1->next;
 
-	while(tmp2->next)
+	while (tmp2->next)
 	{
 		if (tmp1->n < tmp2->n)
+		{
+			while(tmp1->prev != NULL)
+			{
+				if (tmp1->n > tmp2->n)
+				{
+					tmp1 = tmp1->prev;
+				}
+				else
+				{
+					tmp2->next = tmp1;
+					tmp2->prev = tmp1->prev;
+					print_list(*list);
+				}
+				
+			}
+		}
+		else
 		{
 			tmp1 = tmp1->next;
 			tmp2 = tmp2->next;
 		}
-		else
-		{
-			number = tmp1->n;
-			tmp1->n = tmp2->n;
-			tmp2->n = number;
-			tmp1 = *list;
-			tmp2 = tmp1->next;
-			print_list(*list);
-		}
-	
+		
 	}
 }
