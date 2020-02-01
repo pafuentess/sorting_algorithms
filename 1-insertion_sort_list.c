06d128a9aaec26ee3a44022b6b6d8512fc1fb072
pafuentess <pafuentess@unal.edu.co>
2020-02-01 14:52:52 -0500

    el uno funcionando

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -4,38 +4,30 @@
 
 void insertion_sort_list(listint_t **list)
 {
-	listint_t *tmp1, *cpy_tmp1, *reverse, *cpy_list;
+	listint_t *tmp1, *next;
 
 	tmp1 = (*list)->next;
 
-	while (tmp1->next != NULL)
+	while (tmp1)
 	{
-		if (tmp1->n > tmp1->prev->n)
-			tmp1 = tmp1->next;
-		else
+		next = tmp1->next;
+		if (tmp1->prev != NULL && tmp1->prev->n > tmp1->n)
 		{
-			cpy_tmp1 = tmp1;
-			reverse = tmp1;
-			tmp1 = tmp1->next;
-			tmp1->prev = cpy_tmp1->prev;
-			cpy_tmp1->prev->next = tmp1;
-			reverse = cpy_tmp1->prev;
-			while (reverse->prev != NULL)
+			tmp1->prev->next = tmp1->next;
+			if (tmp1->next != NULL)
+				tmp1->next->prev = tmp1->prev;
+			while (tmp1->prev != NULL && tmp1->prev->n > tmp1->n)
 			{
-				if (cpy_tmp1->n < reverse->n && reverse->prev != NULL && cpy_tmp1->n > reverse->prev->n)
-				{
-					reverse->prev->next = cpy_tmp1;
-					reverse->prev = cpy_tmp1;
-					cpy_tmp1->prev = reverse->prev;
-					cpy_tmp1->next = reverse;
-					cpy_list = *list;
-					printf("tmp1 = %d, cpy = %d, rev = %d\n", tmp1->n, cpy_tmp1->n, reverse->n);
-					print_list(cpy_list);
-					printf("hola paula");
-				}
-				reverse = reverse->prev;
+				tmp1->next = tmp1->prev;
+				tmp1->prev = tmp1->prev->prev;
 			}
-			printf("tmp1 = %d, cpy = %d, rev = %d", tmp1->n, cpy_tmp1->n, reverse->n);
+			if (tmp1->prev == NULL)
+				*list = tmp1;
+			else
+				tmp1->prev->next = tmp1;
+			tmp1->next->prev = tmp1;
+			print_list(*list);
 		}
+		tmp1= next;
 	}
 }
